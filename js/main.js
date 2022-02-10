e6522561cdefafa0462b43802153abb53bf3c149
Josiah Victor <victorjosiahm3@gmail.com>
2022-02-10 14:23:40 +0100

    80% mobile menu

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ const mainDashboardPage = document.querySelector('.main-dashboard-page')
 let openNav = 1
 minNav.addEventListener('click', () => {
   const sideNav = document.querySelector('.side-nav')
-  let navLinks = sideNav.querySelectorAll('a')
+  let navLinks = sideNav.querySelectorAll('.nav-link-name')
   let navIcons = sideNav.querySelectorAll('i')
   if (openNav) {
-    minNav.className = 'fas fa-align-justify'
+    minNav.className = 'fas fa-align-justify mobile-menu-btn'
     for (let i = 0; i < navLinks.length; i++) {
       const navLink = navLinks[i]
       navLink.style.display = 'none'
@@ -15,7 +15,7 @@ minNav.addEventListener('click', () => {
     // mainDashboardPage.style.marginLeft = '55px'
     openNav = 0
   } else {
-    minNav.className = 'fas fa-align-left'
+    minNav.className = 'fas fa-align-left mobile-menu-btn'
     for (let i = 0; i < navLinks.length; i++) {
       const navLink = navLinks[i]
       navLink.style.display = 'flex'
